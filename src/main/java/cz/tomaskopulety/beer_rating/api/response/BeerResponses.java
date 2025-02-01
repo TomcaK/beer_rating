6281001a3c6ec490881dcb37f0c66090eabaa4f3
Tomas Kopulety <tomas.kopulety@regiojet.cz>
2025-02-01 11:42:26 +0100

    remove unused import

--- src/main/java/cz/tomaskopulety/beer_rating/api/response/BeerResponses.java
+++ src/main/java/cz/tomaskopulety/beer_rating/api/response/BeerResponses.java
@@ -3,7 +3,6 @@ package cz.tomaskopulety.beer_rating.api.response;
 import java.util.List;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NonNull;
 
